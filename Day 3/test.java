57db0459eb09026fdbacabc77f8188e880d40766
Shubhy9399 <50271931+Shubhy9399@users.noreply.github.com>
2020-06-03 21:33:52 +0530

    Day3

---
+++ Day 3/test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class test {
+	
+	public static void main(String args[]) {
+	String names[]= {"Renu","Rina","Sanju","Raj"};
+	
+	for(String name:names) {
+		if(name.startsWith("R")) {
+			System.out.println(name);
+		}
+	  }
+	}
+}
