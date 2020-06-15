6ae3b4fd1b94a49667473dbc1f285cebd360be20
Shubhangi Pokley <50271931+Shubhy9399@users.noreply.github.com>
2020-06-15 20:48:04 +0530

    WebServices-SOAP_UI

---
+++ Day17/Demo1.java
@@ -0,0 +1,17 @@
+
+public class Demo1 {
+	
+
+	 public int add(int a, int b) {
+		
+		return a+b;
+	}
+       public int sub(int a,int b) {
+    	   return a-b;
+       }
+       
+       public String name(String x) {
+    	   return "Your name is"+x;
+       }
+
+}
